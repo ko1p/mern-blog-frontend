c48ebfedd4a4dea2c923317c63ff0bd953415485
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-07-01 11:54:56 +0300

    refactor FullPost  page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -37,7 +37,7 @@ export const Home = () => {
             (<Post
               id={post._id}
               title={post.title}
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+              imageUrl={post.imageUrl}
               user={post.user}
               createdAt={post.createdAt}
               viewsCount={post.viewsCount}
