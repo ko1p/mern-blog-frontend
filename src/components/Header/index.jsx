6194c2d2651ebe38bef5c2ec301eb29db03888a8
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-07-27 17:54:11 +0300

    fix app title and some meta tags

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -25,7 +25,7 @@ export const Header = () => {
       <Container maxWidth="lg">
         <div className={styles.inner}>
           <Link className={styles.logo} to="/">
-            <div>ARCHAKOV BLOG</div>
+            <div>SHISHKOVA BLOG</div>
           </Link>
           <div className={styles.buttons}>
             {isUserAuth ? (
