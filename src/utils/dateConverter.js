8e0a011927f20cd617d5da5a2e8f7d7ae371be36
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-08-09 16:00:15 +0300

    fix date

--- src/utils/dateConverter.js
+++ src/utils/dateConverter.js
@@ -1,6 +1,6 @@
 export const dateConverter = (date) => {
   const t = new Date(date);
-  return `${("0" + t.getMonth()).slice(-2)}.${("0" + t.getDay()).slice(-2)}.${(
+  return `${("0" + t.getDate()).slice(-2)}.${("0" + (t.getMonth() + 1)).slice(-2)}.${(
     "0" + t.getFullYear()
   ).slice(-2)} ${("0" + t.getHours()).slice(-2)}:${("0" + t.getMinutes()).slice(
     -2
